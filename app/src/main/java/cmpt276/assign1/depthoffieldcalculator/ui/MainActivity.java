4c78948d4ccfc5379d906bfee81a4f5f438c502e
kaitlynnchan <kaitlynnchan@outlook.com>
2020-05-25 15:31:40 -0700

    Moved MainActivity.java into ui package.

---
+++ app/src/main/java/cmpt276/assign1/depthoffieldcalculator/ui/MainActivity.java
@@ -0,0 +1,44 @@
+package cmpt276.assign1.depthoffieldcalculator.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.Collections;
+
+import cmpt276.assign1.depthoffieldcalculator.R;
+import cmpt276.assign1.depthoffieldcalculator.model.Lens;
+import cmpt276.assign1.depthoffieldcalculator.model.LensManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        populateLensManager();
+    }
+
+    private void populateLensManager(){
+        LensManager manager = LensManager.getInstance();
+        manager.add(new Lens("Nikkor", 2.8, 24));
+        manager.add(new Lens("Sony", 1.8, 35));
+        manager.add(new Lens("Canon", 3.5, 80));
+        Lens lens = manager.getLens(0);
+
+        ArrayAdapter<LensManager> adapter = new ArrayAdapter<LensManager>(
+                this,
+                R.layout.lens_item,
+                Collections.singletonList(manager));
+
+        ListView list = findViewById(R.id.listViewLens);
+        list.setAdapter(adapter);
+    }
+}
