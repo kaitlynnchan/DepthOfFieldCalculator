f0bd6ec8fd905d32f1df4ee06d3cf98ac0fcc1bb
kaitlynnchan <kaitlynnchan@outlook.com>
2020-05-26 19:50:48 -0700

    Renamed some IDs and tested cancel button.

--- app/src/main/java/cmpt276/assign1/depthoffieldcalculator/ui/AddLensActivity.java
+++ app/src/main/java/cmpt276/assign1/depthoffieldcalculator/ui/AddLensActivity.java
@@ -3,8 +3,11 @@ package cmpt276.assign1.depthoffieldcalculator.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import cmpt276.assign1.depthoffieldcalculator.R;
@@ -27,5 +30,12 @@ public class AddLensActivity extends AppCompatActivity {
         Intent i = getIntent();
 //        String message = i.getStringExtra(EXTRA_MESSAGE);
 //        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+
+        Button btnCancel = (Button) findViewById(R.id.buttonCancel);
+        btnCancel.setOnClickListener(v -> Toast.makeText(
+                getApplicationContext(),
+                "test cancel button",
+                Toast.LENGTH_LONG).show()
+        );
     }
 }
