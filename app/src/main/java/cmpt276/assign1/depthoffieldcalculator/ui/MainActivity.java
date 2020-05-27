0bfc3c3f1fe0bef6130acc3db6f46f4122d0893c
kaitlynnchan <kaitlynnchan@outlook.com>
2020-05-27 01:12:38 -0700

    Added toast messages.
    Checks to see if intent is working properly

--- app/src/main/java/cmpt276/assign1/depthoffieldcalculator/ui/MainActivity.java
+++ app/src/main/java/cmpt276/assign1/depthoffieldcalculator/ui/MainActivity.java
@@ -3,12 +3,15 @@ package cmpt276.assign1.depthoffieldcalculator.ui;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 import cmpt276.assign1.depthoffieldcalculator.R;
 import cmpt276.assign1.depthoffieldcalculator.model.Lens;
@@ -36,11 +39,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
+        if(resultCode == Activity.RESULT_CANCELED){
+            String message = "Canceled";
+            Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
+            return;
+        }
+
         switch (requestCode){
             case RESULT_CODE_ADD_LENS:
                 String make = data.getStringExtra(AddLensActivity.EXTRA_USER_MAKE);
                 int focalLength = data.getIntExtra(AddLensActivity.EXTRA_USER_FOCAL_lENGTH, 0);
                 double aperture = data.getDoubleExtra(AddLensActivity.EXTRA_USER_APERTURE, 0);
+                String message = "Added lens: " + make + " " + focalLength + "mm F" + aperture;
+//                Snackbar.make(MainActivity.this, message, Snackbar.LENGTH_LONG);
+                Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
                 break;
         }
 
