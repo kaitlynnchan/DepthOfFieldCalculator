ebfcf09c84441d9826fe39c94c7908f63417e9be
kaitlynnchan <kaitlynnchan@outlook.com>
2020-05-27 00:04:28 -0700

    Implemented save feature.
    Need to fix bugs, change result for cancel, and add new lens to lens
    list.

--- app/src/main/java/cmpt276/assign1/depthoffieldcalculator/ui/AddLensActivity.java
+++ app/src/main/java/cmpt276/assign1/depthoffieldcalculator/ui/AddLensActivity.java
@@ -2,40 +2,63 @@ package cmpt276.assign1.depthoffieldcalculator.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.EditText;
 
 import cmpt276.assign1.depthoffieldcalculator.R;
 
 public class AddLensActivity extends AppCompatActivity {
 
-    private static final String EXTRA_MESSAGE = "Extra-message";
-
-    public static Intent makeLaunchIntent(Context c){ //, String message
-        Intent intent = new Intent(c, AddLensActivity.class);
-//        intent.putExtra(EXTRA_MESSAGE, message);
-        return intent;
-    }
+    public static final String EXTRA_USER_MAKE = "user make";
+    public static final String EXTRA_USER_FOCAL_lENGTH = "user focal length";
+    public static final String EXTRA_USER_APERTURE = "user aperture";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_lens);
 
-        Intent i = getIntent();
-//        String message = i.getStringExtra(EXTRA_MESSAGE);
-//        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
-
-        Button btnCancel = (Button) findViewById(R.id.buttonCancel);
-        btnCancel.setOnClickListener(v -> Toast.makeText(
-                getApplicationContext(),
-                "test cancel button",
-                Toast.LENGTH_LONG).show()
-        );
+        setupButtonSave();
+        setupButtonCancel();
     }
+
+    private void setupButtonSave() {
+        Button btnSave = (Button) findViewById(R.id.buttonSave);
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText userMakeEntry = (EditText) findViewById(R.id.editTextMake);
+                String userMake = userMakeEntry.getText().toString();
+
+                EditText userFocalLengthEntry = (EditText) findViewById(R.id.editTextFocalLength);
+                String userFocalLengthData = userFocalLengthEntry.getText().toString();
+                int userFocalLength = Integer.parseInt(userFocalLengthData);
+
+                EditText userApertureEntry = (EditText) findViewById(R.id.editTextAperture);
+                String userApertureData = userApertureEntry.getText().toString();
+                double userAperture = Double.parseDouble(userApertureData);
+
+                Intent intent = new Intent();
+                intent.putExtra(EXTRA_USER_MAKE, userMake);
+                intent.putExtra(EXTRA_USER_FOCAL_lENGTH, userFocalLength);
+                intent.putExtra(EXTRA_USER_APERTURE, userAperture);
+                finish();
+            }
+        });
+    }
+
+    private void setupButtonCancel() {
+        Button btnCancel = (Button) findViewById(R.id.buttonCancel);
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+
 }
--- app/src/main/java/cmpt276/assign1/depthoffieldcalculator/ui/MainActivity.java
+++ app/src/main/java/cmpt276/assign1/depthoffieldcalculator/ui/MainActivity.java
@@ -1,21 +1,14 @@
 package cmpt276.assign1.depthoffieldcalculator.ui;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
-import java.util.Collections;
-import java.util.List;
 
 import cmpt276.assign1.depthoffieldcalculator.R;
 import cmpt276.assign1.depthoffieldcalculator.model.Lens;
@@ -23,21 +16,37 @@ import cmpt276.assign1.depthoffieldcalculator.model.LensManager;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final int RESULT_CODE_ADD_LENS = 42;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        populateLensManager();
+        populateLensList();
 
         FloatingActionButton add = findViewById(R.id.addButton);
         add.setOnClickListener(view -> {
-            Intent i = AddLensActivity.makeLaunchIntent(MainActivity.this); //, "hi"
-            startActivity(i);
+            Intent intent = new Intent(MainActivity.this, AddLensActivity.class);
+            startActivityForResult(intent, RESULT_CODE_ADD_LENS);
         });
     }
 
-    private void populateLensManager(){
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode){
+            case RESULT_CODE_ADD_LENS:
+                String make = data.getStringExtra(AddLensActivity.EXTRA_USER_MAKE);
+                int focalLength = data.getIntExtra(AddLensActivity.EXTRA_USER_FOCAL_lENGTH, 0);
+                double aperture = data.getDoubleExtra(AddLensActivity.EXTRA_USER_APERTURE, 0);
+                break;
+        }
+
+    }
+
+    private void populateLensList(){
         LensManager manager = LensManager.getInstance();
         manager.add(new Lens("Nikkor", 2.8, 24));
         manager.add(new Lens("Sony", 1.8, 35));
@@ -53,4 +62,6 @@ public class MainActivity extends AppCompatActivity {
         ListView list = findViewById(R.id.listViewLens);
         list.setAdapter(adapter);
     }
+
+    // Create updateUI()
 }
