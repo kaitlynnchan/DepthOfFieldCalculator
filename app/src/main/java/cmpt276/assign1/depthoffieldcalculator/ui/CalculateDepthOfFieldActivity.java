13dcd44df175657d3f6ed8f7d40b60e7cedd8b0f
kaitlynnchan <kaitlynnchan@outlook.com>
2020-05-27 13:22:44 -0700

    Created and formatted layout of new activity.

---
+++ app/src/main/java/cmpt276/assign1/depthoffieldcalculator/ui/CalculateDepthOfFieldActivity.java
@@ -0,0 +1,16 @@
+package cmpt276.assign1.depthoffieldcalculator.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import cmpt276.assign1.depthoffieldcalculator.R;
+
+public class CalculateDepthOfFieldActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculate_depth_of_field);
+    }
+}
